d712aca6291fbd36fe72976ab585ce090e25f7c1
Domin <a0975142932@gmail.com>
2026-09-10 19:53:09 +0800

    add focaltech convert code

--- main.py
+++ main.py
@@ -1,89 +1,85 @@
 import tkinter as tk
-from dataclasses import replace
-from idlelib.editor import keynames
-from shlex import join
-from tkinter import filedialog, scrolledtext, ttk
+from tkinter import filedialog, scrolledtext, ttk, messagebox
 from PIL import Image, ImageTk
 
 class TextConvertApp(tk.Tk):
 
-    def __init__(windows):
+    def __init__(self):
         super().__init__()
-        windows.title("宜特科技轉code程式")
-        windows.iconbitmap("icon.ico")
-        windows.geometry("1200x720")
-        windows.code_file = ""
-        #windows.configure(background="white")
-        windows.build_ui()
+        self.title("宜特科技轉code程式")
+        self.iconbitmap("icon.ico")
+        self.geometry("1200x720")
+        self.code_file = ""
+        self.build_ui()
 
-    def build_ui(windows):
-        tool_bar = tk.Frame(windows)
+    def build_ui(self):
+        tool_bar = tk.Frame(self)
         tool_bar.grid(row=0, column=0, sticky="ew")
 
         logo_image = Image.open("logo.png").resize((80, 40))
-        windows.logo_image = ImageTk.PhotoImage(logo_image)
-        ttk.Label(tool_bar, image=windows.logo_image).pack(side="left")
-        windows.file_entry = ttk.Entry(tool_bar, width=103)
-        windows.file_entry.pack(padx=10, pady=20, side="left")
-        ttk.Button(tool_bar, text="讀取檔案", command=windows.select_file).pack(pady=20, side="left")
-        windows.company = tk.StringVar(value="選取公司")
-        windows.company_type = (ttk.Combobox(tool_bar, textvariable=windows.company, values=["瑞鼎", "集創", "敦泰"], state="readonly"))
-        windows.company_type.pack(padx=10, pady=20, side="left")
-        ttk.Button(tool_bar, text="開始轉檔", command=windows.convert_file).pack(pady=20, side="left")
+        self.logo_image = ImageTk.PhotoImage(logo_image)
+        ttk.Label(tool_bar, image=self.logo_image).pack(side="left")
+        self.file_entry = ttk.Entry(tool_bar, width=103)
+        self.file_entry.pack(padx=10, pady=20, side="left")
+        ttk.Button(tool_bar, text="讀取檔案", command=self.select_file).pack(pady=20, side="left")
+        self.company = tk.StringVar(value="選取公司")
+        self.company_type = (ttk.Combobox(tool_bar, textvariable=self.company, values=["瑞鼎", "集創", "敦泰"], state="readonly"))
+        self.company_type.pack(padx=10, pady=20, side="left")
+        ttk.Button(tool_bar, text="開始轉檔", command=self.convert_file).pack(pady=20, side="left")
 
-        windows.text_frame = tk.Frame(windows)
-        windows.text_frame.grid(row=1, column=0, sticky="nsew", padx=10, pady=20)
-        windows.text_frame.columnconfigure(0, weight=1, uniform="half")
-        windows.text_frame.columnconfigure(1, weight=1, uniform="half")
-        windows.text_frame.rowconfigure(1, weight=1)
+        self.text_frame = tk.Frame(self)
+        self.text_frame.grid(row=1, column=0, sticky="nsew", padx=10, pady=20)
+        self.text_frame.columnconfigure(0, weight=1, uniform="half")
+        self.text_frame.columnconfigure(1, weight=1, uniform="half")
+        self.text_frame.rowconfigure(1, weight=1)
 
-        left_frame = tk.Frame(windows.text_frame)
+        left_frame = tk.Frame(self.text_frame)
         left_frame.rowconfigure(1, weight=1)
         left_frame.columnconfigure(0, weight=1)
         left_frame.pack(fill="both", expand=True, side="left", padx=(0, 5))
 
         ttk.Label(left_frame, text="原始code").grid(row=0, column=0, sticky="w")
-        windows.left_text_area = scrolledtext.ScrolledText(left_frame, wrap="word")
-        windows.left_text_area.grid(row=1, column=0, sticky="nsew")
+        self.left_text_area = scrolledtext.ScrolledText(left_frame, wrap="word")
+        self.left_text_area.grid(row=1, column=0, sticky="nsew")
 
-        right_frame = tk.Frame(windows.text_frame)
+        right_frame = tk.Frame(self.text_frame)
         right_frame.rowconfigure(1, weight=1)
         right_frame.columnconfigure(1, weight=1)
         right_frame.pack(fill="both", expand=True, side="left", padx=(5, 0))
 
         ttk.Label(right_frame, text="轉檔code").grid(row=0, column=1, sticky="w")
-        windows.right_text_area = scrolledtext.ScrolledText(right_frame, wrap="word")
-        windows.right_text_area.grid(row=1, column=1, sticky="nsew")
+        self.right_text_area = scrolledtext.ScrolledText(right_frame, wrap="word")
+        self.right_text_area.grid(row=1, column=1, sticky="nsew")
 
         #視窗往下拉也會放大
-        windows.rowconfigure(1, weight=1)
-        windows.columnconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.columnconfigure(0, weight=1)
 
-        save_bar = tk.Frame(windows)
+        save_bar = tk.Frame(self)
         save_bar.grid(row=2, column=0, sticky="ew")
-        ttk.Button(save_bar, text="儲存檔案", command=windows.save_file).pack(pady=20)
+        ttk.Button(save_bar, text="儲存檔案", command=self.save_file).pack(pady=20)
 
 
-    def select_file(windows):
+    def select_file(self):
         file_path = filedialog.askopenfilename(
             title = "選擇客戶 TXT 檔",
             filetypes = [("文字檔", "*.txt")]
         )
         if file_path:
-            windows.file_entry.delete(0, tk.END)
-            windows.file_entry.insert(0, file_path)
-            windows.code_file = file_path
+            self.file_entry.delete(0, tk.END)
+            self.file_entry.insert(0, file_path)
+            self.code_file = file_path
             try:
                 with open(file_path, "r", encoding="utf-8") as file:
                     content = file.read()
             except (OSError, UnicodeDecodeError) as error:
                 content = f"無法讀取檔案：{error}"
 
-            windows.left_text_area.delete("1.0", tk.END)
-            windows.left_text_area.insert("1.0", content)
+            self.left_text_area.delete("1.0", tk.END)
+            self.left_text_area.insert("1.0", content)
 
-    def save_file(windows):
-        content = windows.right_text_area.get("1.0", tk.END)
+    def save_file(self):
+        content = self.right_text_area.get("1.0", tk.END)
         file_path = filedialog.asksaveasfilename(
             title="儲存 TXT 檔",
             defaultextension=".txt",
@@ -97,7 +93,8 @@ class TextConvertApp(tk.Tk):
         except OSError as error:
             print(f"無法儲存檔案：{error}")
 
-    def raydiumn_code(windows, lines):
+    def raydiumn_code(self, lines):
+        raydiumn_code = ""
         for index, line in enumerate(lines):
             if "SSD WRITE" in line:
                 raydiumn_code = "\n".join(lines[index:])
@@ -136,26 +133,68 @@ class TextConvertApp(tk.Tk):
 
         return "\n".join(result_lines)
 
-    def convert_file(windows):
-        with open(windows.code_file, "r", encoding="utf-8") as file:
+    def focaltech_code(self, lines):
+        found_line = 0
+        focaltech_code = ""
+        for index, line in enumerate(lines, 1):
+            if "Video Mode Enable" in line:
+                found_line = index
+                break
+        for index, line in enumerate(lines):
+            if "LCD initial code Start" in line:
+                focaltech_code = "\n".join(lines[index:found_line-1])
+                break
+
+        replacement = {
+            "/": " #",
+            ");": "",
+            ",": " ",
+            "GEN_WR(": "mipi.write 0x39 ",
+            ")": "",
+            "delayms_pc(": "delay ",
+        }
+
+        result_lines = []
+
+        for line in focaltech_code.splitlines():
+            for keyword, new_text in replacement.items():
+                before_hash, hash_mark, after_hash = line.partition("#")
+
+                # 只取代 # 前面的內容
+                before_hash = before_hash.replace(keyword, new_text)
+
+                # # 後面的內容保持原樣
+                line = before_hash + hash_mark + after_hash
+
+            result_lines.append(line)
+
+        return "\n".join(result_lines)
+
+    def convert_file(self):
+        if not self.code_file:
+            messagebox.showwarning("提示", "請載入code！")
+            return
+        with open(self.code_file, "r", encoding="utf-8") as file:
             code = file.read()
 
         lines = code.splitlines()
-
-        if windows.company.get() == "瑞鼎":
-            convert_code = windows.raydiumn_code(lines)
-
+        convert_code = ""
+        if self.company.get() == "瑞鼎":
+            convert_code = self.raydiumn_code(lines)
+        elif self.company.get() == "敦泰":
+            convert_code = self.focaltech_code(lines)
+        else:
+            messagebox.showwarning("提示", "請選取公司！")
+            return
         with open("mipi_setting.TXT", "r", encoding="utf-8") as file1, \
              open("mipi_setting2.TXT", "r", encoding="utf-8") as file2:
             file1_content = file1.read()
             file2_content = file2.read()
-            # for line in convert_code:
-            #     file1.write(line)
 
         final_code = f"{file1_content}\n{convert_code}\n{file2_content}"
 
-        windows.right_text_area.delete("1.0", tk.END)
-        windows.right_text_area.insert("1.0", final_code)
+        self.right_text_area.delete("1.0", tk.END)
+        self.right_text_area.insert("1.0", final_code)
 
 
 # ---- 程式執行入口 ----
